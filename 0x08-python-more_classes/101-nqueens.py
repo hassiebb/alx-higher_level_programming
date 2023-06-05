3b20f9c56dd78fbf8ac1a36e933d808540a6f9fd
Mohammad Hassieb <mohammadhassieb@gmail.com>
2023-06-05 20:52:11 +0300

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
 """
+
 This module contains an algorithm that resolves the N-Queen puzzle
 using backtracking
+
 """
 
 
 def isSafe(m_queen, nqueen):
     """ Method that determines if the queens can or can't kill each other
+
     Args:
         m_queen: array that has the queens positions
         nqueen: queen number
+
     Returns:
         True: when queens can't kill each other
         False: when some of the queens can kill
+
     """
+
     for i in range(nqueen):
+
         if m_queen[i] == m_queen[nqueen]:
             return False
+
         if abs(m_queen[i] - m_queen[nqueen]) == abs(i - nqueen):
             return False
 
@@ -25,29 +33,40 @@ def isSafe(m_queen, nqueen):
 
 def print_result(m_queen, nqueen):
     """ Method that prints the list with the Queens positions
+
     Args:
         m_queen: array that has the queens positions
         nqueen: queen number
+
     """
+
     res = []
+
     for i in range(nqueen):
         res.append([i, m_queen[i]])
+
     print(res)
 
 
 def Queen(m_queen, nqueen):
     """ Recursive function that executes the Backtracking algorithm
+
     Args:
         m_queen: array that has the queens positions
         nqueen: queen number
+
     """
+
     if nqueen is len(m_queen):
         print_result(m_queen, nqueen)
         return
+
     m_queen[nqueen] = -1
 
     while((m_queen[nqueen] < len(m_queen) - 1)):
+
         m_queen[nqueen] += 1
+
         if isSafe(m_queen, nqueen) is True:
 
             if nqueen is not len(m_queen):
@@ -55,11 +74,13 @@ def Queen(m_queen, nqueen):
 
 
 def solveNQueen(size):
-
     """ Function that invokes the Backtracking algorithm
+
     Args:
         size: size of the chessboard
+
     """
+
     m_queen = [-1 for i in range(size)]
 
     Queen(m_queen, 0)
@@ -75,7 +96,7 @@ if __name__ == '__main__':
 
     try:
         size = int(sys.argv[1])
-    except Exception:
+    except:
         print("N must be a number")
         sys.exit(1)
 
