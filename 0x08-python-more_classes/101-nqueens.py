cf94a00b02f281fa51a1c13d4205a91a5b7ca801
Mohammad Hassieb <mohammadhassieb@gmail.com>
2023-06-07 14:12:46 +0300

    Update 101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -92,9 +92,9 @@ if __name__ == '__main__':
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-
     if size < 4:
         print("N must be at least 4")
         sys.exit(1)
-
     solveNQueen(size)
+
+    
