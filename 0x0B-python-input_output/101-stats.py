da0c49ab58ec821426f2e784652d2af2f9763c83
Mohammad Hassieb <mohammadhassieb@gmail.com>
2023-06-14 20:49:58 +0300

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -24,6 +24,7 @@ if __name__ == "__main__":
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
     count = 0
 
+
     try:
         for line in sys.stdin:
             if count == 10:
@@ -47,7 +48,7 @@ if __name__ == "__main__":
 
         print_stats(size, status_codes)
 
+
     except KeyboardInterrupt:
         print_stats(size, status_codes)
         raise
-
