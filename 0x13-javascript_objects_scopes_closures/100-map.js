e93ae10b4c90a0092ec2da9b7dc7f48c1e85381b
Mohammad Hassieb <mohammadhassieb@gmail.com>
2023-08-27 11:47:37 +0300

    Update 100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((item, index) => item * index));
+const data = require('./100-data');
+
+const initialList = data.list;
+const newList = initialList.map((value, index) => value * index);
+
+console.log(initialList);
+console.log(newList);
